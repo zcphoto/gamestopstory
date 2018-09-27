64a264ac1e2f2b881998c0699c3978adda8073e6
William Zhao <williamzha.11772@redlandsschools.net>
2018-09-27 15:10:05 +0000

    story

---
+++ story.py
@@ -0,0 +1,5 @@
+p1 = input('You recieve a text from your friend asking, "Do you wanna get a new game from GameStop?"')
+print("Yes or No")
+if p1 = ("Yes, Y, y"):
+    print("You decide to go but realize that you lost your car keys a while ago")
+    print()
